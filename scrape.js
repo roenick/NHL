ae2971ca63606873a4868bfad87b6808e1adf422
rendalli <roenick@gmx.net>
2017-07-23 23:43:34 +0200

    tes2

--- scrape.js
+++ scrape.js
@@ -4,6 +4,6 @@ var cheerio = require('cheerio');
 request('http://www.nhl.com/scores/htmlreports/20162017/PL030411.HTM', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
-/*Hallo*/
+/*Hallo noch etwas mehr Kommentar */
     }
 });
