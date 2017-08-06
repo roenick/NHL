5ed77eea2c3ffe6c3d9a9a8964b6854bc8aec3f8
rendalli <roenick@gmx.net>
2017-08-07 01:21:35 +0200

    scraping II

--- scrape.js
+++ scrape.js
@@ -1,51 +1,84 @@
 (async () => {
 
-    var request = require('request');
-    var cheerio = require('cheerio');
-    var fs = require('fs'); // Für Testzwecke
-    const {Client} = require('pg');
+    let request = require('request');
+let cheerio = require('cheerio');
+let fs = require('fs'); // Für Testzwecke
+const {Client} = require('pg');
+const named = require('node-postgres-named');
 
 
-    const client = new Client({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'NHL',
-        password: 'iceman11',
-        port: 5432,
-    });
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'NHL',
+    password: 'iceman11',
+    port: 5432,
+});
 
-    await client.connect();
+named.patch(client);
 
-    $ = cheerio.load(fs.readFileSync('PlayByPlay.htm'));
+await client.connect();
 
-    /*
-    // Files: http://www.nhl.com/scores/htmlreports/20162017/PL02001 bis PL021230.HTM
+$ = cheerio.load(fs.readFileSync('PlayByPlay.htm'));
 
-    request('http://www.nhl.com/scores/htmlreports/20162017/PL030411.HTM', function (error, response, html) {
-         if (!error && response.statusCode == 200) {
-             var $ = cheerio.load(html);
-         }
-    });
-    */
+/*
+// Files: http://www.nhl.com/scores/htmlreports/20162017/PL02001 bis PL021230.HTM
 
-    async function storeGameDetails() {
-        let ATeam = $('#Visitor td').last().html().split("<br>")[0]
-        let AScore = $('#Visitor table td').eq(1).text()
-        let HScore = $('#Home table td').eq(1).text()
-        let HTeam = $('#Home td').last().html().split("<br>")[0]
-        let GameID = $('#GameInfo tr').eq(-2).text().split(" ")[1];
-        let GDate = $('#GameInfo tr').eq(-5).text();
-        let GType = $('#GameInfo tr').eq(-6).text();
+request('http://www.nhl.com/scores/htmlreports/20162017/PL030411.HTM', function (error, response, html) {
+     if (!error && response.statusCode == 200) {
+         var $ = cheerio.load(html);
+     }
+});
+*/
 
-        const query = {
-            text: 'INSERT INTO games VALUES($1, $2, $3, $4, $5, $6, $7)',
-            values: [GameID, HTeam, ATeam, HScore, AScore, GType, GDate],
+async function storeGameDetails() {
+    let ATeam = $('#Visitor td').last().html().split("<br>")[0]
+    let AScore = $('#Visitor table td').eq(1).text()
+    let HScore = $('#Home table td').eq(1).text()
+    let HTeam = $('#Home td').last().html().split("<br>")[0]
+    let GameID = $('#GameInfo tr').eq(-2).text().split(" ")[1];
+    let GDate = $('#GameInfo tr').eq(-5).text();
+    let GType = $('#GameInfo tr').eq(-6).text();
+
+    const query = {
+        text: 'INSERT INTO games VALUES($GameID, $HTeam, $ATeam, $HScore, $AScore, $GType, $GDate)',
+        values: {
+            'GameID': GameID,
+            'HTeam': HTeam,
+            'ATeam': ATeam,
+            'HScore': HScore,
+            'AScore': AScore,
+            'GType': GType,
+            'GDate': GDate
         }
-        const res = await client.query(query);
     }
+    const res = await client.query(query);
+}
 
-    await storeGameDetails();
-    await client.end();
+async function storePlays() {
+    let actualRow = $('tr.evenColor').eq(0).find('td') //Change this to each (tr.evenColor).each...
+    let Period = actualRow.eq(1).text() //Period in second td
+    let TimeElapsed = actualRow.eq(3).html().split("<br>")[0] //TimeElapsed in fourth td
+    let EventName = actualRow.eq(4).text() // Event-Name
+    let VisitorPlayersOnIce = []
+    let HomePlayersOnIce = []
+    let VisitorOnIce = actualRow.eq(6).find('table table') // Visitor on ice
+    VisitorOnIce.each(function (index, element) {
+        let PlayerName = $(element).find('font').attr('title').split(" - ")[1]
+        let PlayerNumberAndPos = $(element).text().replace(/(\r\n|\n|\r)/gm,"");
+        let PlayerNumber = PlayerNumberAndPos.split(/(\d+)/)[1]
+        let PlayerPos = PlayerNumberAndPos.split(/(\d+)/)[2]
+        VisitorPlayersOnIce.push([PlayerName,PlayerNumber,PlayerPos])
+    });
+
+    console.log(VisitorPlayersOnIce);
+}
+
+//await storeGameDetails();
+await storePlays();
+
+await client.end();
 
 
 })().catch(e => setImmediate(() => { throw e }));
+
